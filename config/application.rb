1d910601e4b9d115e51248ae79c52605688b62d1
Arif Alim <arifalim9@gmail.com>
2020-08-06 16:40:29 -0400

    added config.assets.initialize_on_precompile = false in application.rb to avoid connecting to a db

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Titan
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Assets should compile without your app attempting to connect to a database
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
