1aa9c255f4e64e80ff7e95bf91bbe85a14efb5e3
Maxim Zelenkin <nudepatch@gmail.com>
2020-08-31 23:46:27 +0500

    Fix ignored validation on submit contact form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 import("jquery")
 import("js/custom")
 import("js/contact_form")
+import("js/validator")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
