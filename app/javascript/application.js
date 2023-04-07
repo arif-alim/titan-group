cb922d6ae35699b394635568602bb135c6ed9fbc
waleed <waleedarshad72@gmail.com>
2023-04-07 16:53:35 +0500

    Updates

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "../../node_modules/@fortawesome/fontawesome-free/js/all";
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "../../node_modules/jquery/dist/jquery";
-import "../../node_modules/scrollmagic/scrollmagic";
+import "jquery";
+import "scrollmagic";
 import "./js/custom";
 import "./js/contact_form";
 import "./js/validator";
