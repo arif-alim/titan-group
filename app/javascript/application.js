5c1e82676321ad1b8507513d449db90b642be8e2
waleed <waleedarshad72@gmail.com>
2023-04-07 16:41:58 +0500

    Upgrade rails v3 inprogress

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+import "../../node_modules/@fortawesome/fontawesome-free/js/all";
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// import "../../node_modules/jquery.js";
-// import("../../node_modules/scrollmagic");
-// import("./js/custom");
-// import("./js/contact_form");
-// import("./js/validator");
+import "../../node_modules/jquery/dist/jquery";
+import "../../node_modules/scrollmagic/scrollmagic";
+import "./js/custom";
+import "./js/contact_form";
+import "./js/validator";
